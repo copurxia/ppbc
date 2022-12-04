73117b7d485af09fba14bd17432bd688d5bda89c
copurxia <copur@qq.com>
2022-12-04 18:45:56 +0800

    fix plantname

--- ppba.py
+++ ppba.py
@@ -19,16 +19,19 @@ while True:
         driver.get(line)
         count += 1
         sleep(5)
+        plant_name = driver.find_element(
+            By.CSS_SELECTOR, ".divpa2> .fl> div:nth-child(2)").text
+        print("Plant Name =", plant_name)
         # 滚动到底部
         temp_height = 0
         while True:
             driver.execute_script("window.scrollBy(0,1000)")
-            sleep(5)
+            sleep(1)
             check_height = driver.execute_script(
                 "return document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;")
             if check_height == temp_height:
                 break
-        temp_height = check_height
+            temp_height = check_height
         driver.implicitly_wait(0.5)
         plants = driver.find_elements(
             By.CSS_SELECTOR, ".namew.fl> a:nth-child(1)")
@@ -38,14 +41,13 @@ while True:
             plantar.append(plant.get_attribute("href"))
         for plant_detail in plantar:
             driver.get(plant_detail)
+            print("Plant Detail =", plant_detail)
             sleep(2)
             driver.implicitly_wait(0.5)
             plant_time = driver.find_element(
                 By.CSS_SELECTOR, ".divback21> div:nth-child(3)").text[5:]
             plant_img = driver.find_element(
                 By.CSS_SELECTOR, "#viewer2> img").get_attribute("src")
-            plant_name = driver.find_element(
-                By.CSS_SELECTOR, "#txt_classsys> a:nth-child(3)> b").text
             print(plant_name, "\n", plant_time, plant_img)
             # 保存图片
             if not os.path.exists("img/" + str(count) + " " + plant_name):
