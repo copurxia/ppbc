a976f3dbd87f363e4e0cf9d68eac11d1e8534832
copurxia <copur@qq.com>
2022-12-07 10:50:47 +0800

    fix color

--- color.py
+++ color.py
@@ -2,6 +2,9 @@ import os
 import datetime
 import copy
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from palettable.cartocolors.sequential import DarkMint_4  # type: ignore
 from haishoku.haishoku import Haishoku
 
 
@@ -11,15 +14,18 @@ def date_num(y, m, d):
     return (d1-d2).days
 
 
-r = np.zeros((366, 2600), dtype=int)
-g = np.zeros((366, 2600), dtype=int)
-b = np.zeros((366, 2600), dtype=int)
+r = np.zeros((2600, 366), dtype=int)
+g = np.zeros((2600, 366), dtype=int)
+b = np.zeros((2600, 366), dtype=int)
 
 plants = os.listdir("img")
+color_map = []
 
 for plant in plants:
     print(plant)
+    index_count = np.zeros(366, dtype=int)
     pl_imgs = os.listdir("img\\"+plant)
+    pl_imgs.sort()
     for pl_img in pl_imgs:
         print(plant.split(" ")[1])
         haishoku = Haishoku.loadHaishoku("img\\"+plant+"\\"+pl_img)
@@ -27,14 +33,29 @@ for plant in plants:
         datear = pl_img.split(" ")[1].split("-")
         arindex = date_num(
             int(datear[0]), int(datear[1]), int(datear[2]))
+        while index_count[arindex] > 3:
+            arindex = arindex+1
         print("数组序号：", arindex)
+        index_count[arindex] = index_count[arindex]+1
         print("时间：", pl_img.split(" ")[2].split(".")[0].replace(";", ":"))
         print("主色：", haishoku.dominant)
         for i in range(0, 2600):
-            if r[i][arindex] == 0 and g[i][arindex] == 0 and b[i][arindex] == 0:
+            if r[i][arindex] == 0 and g[i][arindex] == 0 and b[i][arindex] == 0:  # type: ignore
                 r[i][arindex] = copy.copy(haishoku.dominant[0])  # type: ignore
                 g[i][arindex] = copy.copy(haishoku.dominant[1])  # type: ignore
                 b[i][arindex] = copy.copy(haishoku.dominant[2])  # type: ignore
+                color_str = "#"+'{:02X}'.format(r[i][arindex]) + \
+                    '{:02X}'.format(g[i][arindex]) + \
+                    '{:02X}'.format(b[i][arindex])
+                print("colorstr=", color_str)
                 print("i=", i)
                 break
         print("")
+
+x = np.arange(-0.5, 366, 1)  # len = 2600
+y = np.arange(-0.5, 50, 1)  # len = 366
+
+fig, ax = plt.subplots()
+# type: ignore
+ax.pcolormesh(x, y, g[:100], cmap=DarkMint_4.mpl_colormap)
+plt.show()
--- visual.py
+++ visual.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from palettable.cartocolors.sequential import DarkMint_4  # type: ignore
 
 
 np.random.seed(19680801)
-Z = np.random.rand(100, 165)
-x = np.arange(-0.5, 165, 1)  # len = 200
-y = np.arange(-0.5, 100, 1)  # len = 300
+Z = np.random.rand(126, 136)
+x = np.arange(-0.5, 136, 1)  # len = 200
+y = np.arange(-0.5, 126, 1)  # len = 300
 
 
 fig, ax = plt.subplots()
-ax.pcolormesh(x, y, Z)
+ax.pcolormesh(x, y, Z, cmap=DarkMint_4.mpl_colormap)
+print(type(DarkMint_4.mpl_colormap))
+print(DarkMint_4.mpl_colormap)
 plt.show()
